416f83cc48cc1346c1800de66cdfcdfd1c450b5d
TobiasBergg <cph-tb266@cphbusiness.dk>
2022-11-03 09:53:33 +0100

    03-11

--- src/Stream.java
+++ src/Stream.java
@@ -1,22 +1,18 @@
 import java.util.ArrayList;
 
 public class Stream {
+
     private User currentUser;
     private Startmenu startmenu;
     public static ArrayList <IMedia> Movies;
     public static ArrayList <IMedia> Series;
-   public Stream()
-   {
+   public Stream() {
 
    }
-    public void start()
-    {
+    public void start() {
 
     }
-    public void watch(IMedia media)
-    {
+    public void watch(IMedia media) {
 
     }
-
-
 }
