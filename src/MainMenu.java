9fa9a543b9195a7f9425f764a0e0c6609cb28bc1
Bastian-cph <cph-bh213@cphbusiness.dk>
2022-11-04 10:48:38 +0100

    comments for mive and mainmenu class

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,20 +1,42 @@
 import java.util.ArrayList;
 
 public class MainMenu {
+private User user;
 
-    public IMedia Search(String mediaName){
+    public MainMenu(User user)
+    {
+        this.user = user;
+    }
+
+    public IMedia Search(String mediaName)
+    {
         return new Movie("1", "1", new ArrayList<>(), 12);
+        /*
+        search for a movie/series name in the csv files
+         */
     }
 
-    public ArrayList<IMedia> searchCategory(String category){
+
+    public ArrayList<IMedia> searchCategory(String category)
+    {
         return new ArrayList<>();
+        /*search for a movie/series category with a string
+        */
+
     }
 
-    public ArrayList<IMedia> getUsersWatchedMedia(User user){
+
+    public ArrayList<IMedia> getUsersWatchedMedia(User user)
+    {
         return new ArrayList<>();
+        //get users watched media from User user "".
     }
 
-    public ArrayList<IMedia> getUsersSavedMedia(User user){
+    //Add to a
+    public ArrayList<IMedia> getUsersSavedMedia(User user)
+    {
         return new ArrayList<>();
+        //get users saved media from User user
     }
+
 }
