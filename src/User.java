0f7f5f9e22c867e6a9bd33b88de6ddbfea69045a
TobiasTheDanish <t.hc199811@gmail.com>
2022-11-07 23:36:09 +0100

    Added functions to init media lists

--- src/User.java
+++ src/User.java
@@ -11,6 +11,47 @@ public class User
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        watchedMedia = initWatchedMedia(username);
+        savedMedia = initSavedMedia(username);
+    }
+
+    private ArrayList<IMedia> initWatchedMedia(String username)
+    {
+        ArrayList<IMedia> returnList = new ArrayList<>();
+        String data = FileIO.getSingleUserData(username);
+        if (data == null) return new ArrayList<>();
+
+        String watchedMediaStr = data.split(",")[2];
+        if (watchedMediaStr.equalsIgnoreCase("null")) return new ArrayList<>();
+
+        String[] watchedMedia = watchedMediaStr.split(":");
+
+        for (String mediaName : watchedMedia)
+        {
+            returnList.add(MainMenu.Search(mediaName));
+        }
+
+        return returnList;
+    }
+
+    private ArrayList<IMedia> initSavedMedia(String username)
+    {
+        ArrayList<IMedia> returnList = new ArrayList<>();
+
+        String data = FileIO.getSingleUserData(username);
+        if (data == null) return new ArrayList<>();
+
+        String savedMediaStr = data.split(",")[3];
+        if (savedMediaStr.equalsIgnoreCase("null")) return new ArrayList<>();
+
+        String[] savedMedia = savedMediaStr.split(":");
+
+        for (String mediaName : savedMedia)
+        {
+            returnList.add(MainMenu.Search(mediaName));
+        }
+
+        return returnList;
     }
 
     public ArrayList<IMedia> getWatchedMedia() {
@@ -23,13 +64,13 @@ public class User
 
     public void addToWatchedMedia (IMedia media){
         watchedMedia.add(media);
-        TextUI.displayMessage(media + " has been added to watched media.");
+        TextUI.displayMessage(media.getName() + " has been added to watched media.");
         //Add the selected movie to: ArrayList<IMedia> getWatchedMedia()
     }
 
     public void addToSavedMedia (IMedia media){
         savedMedia.add(media);
-        TextUI.displayMessage(media + " has been added to saved media.");
+        TextUI.displayMessage(media.getName() + " has been added to saved media.");
         //Add the selected movie to: ArrayList<IMedia> getSavedMedia()
     }
 
