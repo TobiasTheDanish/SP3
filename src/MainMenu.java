8df4bfbb8c786014ed45d63d1dc4e76bcb6b996d
Bastian-cph <cph-bh213@cphbusiness.dk>
2022-11-09 15:25:24 +0100

    Comments 09-11-22

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,66 +1,60 @@
 import java.util.ArrayList;
 
 public class MainMenu {
-
-    public static IMedia Search(String mediaName)
-    {
-        for (IMedia m: Application.movies)
-        {
-             if(m.getName().equalsIgnoreCase(mediaName))
-             {
+    //Making a function where we are able to search by name in our arraylist
+    //Does not make any difference whether it's a movie or a series
+    public static IMedia Search(String mediaName) {
+        //Using a foreach loop to be able to search in our arraylist
+        // will return the name of the movie if it's in the list
+        for (IMedia m: Application.movies) {
+             if(m.getName().equalsIgnoreCase(mediaName)) {
                  return m;
              }
 
         }
-        for (IMedia s : Application.series)
-        {
-            if(s.getName().equalsIgnoreCase(mediaName))
-            {
+        //Using a foreach loop to be able to search in our arraylist
+        // will return the name of the movie if it's in the list
+        for (IMedia s : Application.series) {
+            if(s.getName().equalsIgnoreCase(mediaName)) {
                 return s;
             }
         }
         return null;
     }
 
-
-    public static ArrayList<IMedia> searchCategory(String category)
-    {
+    //Making a function where we are able to search by category in our arraylist
+    public static ArrayList<IMedia> searchCategory(String category) {
         ArrayList<IMedia> searchResults = new ArrayList<>();
-        for (IMedia m : Application.movies)
-        {
-            for (String cat : m.getCategories())
-            {
-                if (cat.equalsIgnoreCase(category))
-                {
+        //Making a foreach loop for the movie categories, will be called by the application class
+
+        for (IMedia m : Application.movies) {
+            for (String cat : m.getCategories()) {
+                if (cat.equalsIgnoreCase(category)) {
                     searchResults.add(m);
 
                 }
             }
         }
-
-        for (IMedia s : Application.series)
-        {
-            for (String cat : s.getCategories())
-            {
-                if (cat.equalsIgnoreCase(category))
-                {
+        // Making one for the series as well
+        for (IMedia s : Application.series) {
+            for (String cat : s.getCategories()) {
+                if (cat.equalsIgnoreCase(category)) {
                     searchResults.add(s);
                 }
             }
         }
+        //Returns movies in the chosen category
         return searchResults;
     }
 
 
-    public static ArrayList<IMedia> getUsersWatchedMedia(User user)
-    {
+    public static ArrayList<IMedia> getUsersWatchedMedia(User user) {
         return user.getWatchedMedia();
 
     }
 
-    //Add to a
-    public static ArrayList<IMedia> getUsersSavedMedia(User user)
-    {
+
+    public static ArrayList<IMedia> getUsersSavedMedia(User user) {
         return user.getSavedMedia();
 
     }
